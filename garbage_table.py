658d145bd99ed215399bd22eaf811444da88a48e
Ishan <ishans@usc.edu>
2018-03-31 16:56:14 -0700

    what

--- garbage_table.py
+++ garbage_table.py
@@ -11,15 +11,16 @@ csv_data_classes = csv.reader(file('data/new_files/new_su2015.csv'))
 csv_data = csv.reader(file('data/new_files/new_su2015.csv'))
 csv_data2 = csv.reader(file('data/new_files/new_su2015.csv'))
 
-classes = []
+classes = dict()
 dict = {'4':'num_A','3.7':'num_Am','3.3':'num_Bp','3':'num_B','2.7':'num_Bm', \
 	'2.3':'num_Cp','2':'num_C','1.7':'num_Cm','1.3':'num_Dp','1':'num_D', \
 	'0.7':'num_Dm','0':'num_F','Pass':'num_pass','Not Pass':'num_nopass'}
 
 row_header = next(csv_data_classes)
 for row in csv_data_classes:
-	if classes.count(row[4]) == 0:
-		classes.append(row[4])
+	if classes.find(row[3]).count(row[4]) == 0:
+		#classes.append(row[4])
+		classes[row[3]].add(row[4])
 		cursor.execute('INSERT INTO ratemyclass(course_number,professors,course_subject,course_name)' \
 			'VALUES(%s, %s, %s, %s)', (row[4],row[1],row[3],row[6]))
 
